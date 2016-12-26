d2cd805a6f1b3724c0e4df8c6b8b4b913fb4c6dc
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-26 12:38:04 +0100

    Added `EvalExceptionInterface`

---
+++ src/EvalExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Evaluable;
+
+/**
+ * Interface for exceptions thrown as a consequence of an evaluation problem.
+ *
+ * @since [*next-version*]
+ */
+interface EvalExceptionInterface
+{
+}
---
+++ test/functional/EvalExceptionInterfaceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dhii\Evaluable\Test;
+
+use \Dhii\Evaluable\EvalExceptionInterface;
+use \Xpmock\TestCase;
+
+/**
+ * Tests {@see \Dhii\Evaluable\EvalExceptionInterface}.
+ *
+ * @since [*next-version*]
+ */
+class EvalExceptionInterfaceTest extends TestCase
+{
+
+    /**
+     * The name of the test subject.
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\\Evaluable\\EvalExceptionInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return EvalExceptionInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @covers \Dhii\Espresso\EvalExceptionInterface
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(static::TEST_SUBJECT_CLASSNAME, $subject);
+    }
+
+}
